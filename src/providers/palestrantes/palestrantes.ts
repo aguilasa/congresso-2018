a2cfecb54daffde2ea61b01d3c5bab7a6f3c05f7
Ingmar Aguiar <ingmar.aguiar@senior.com.br>
2017-09-13 15:07:47 -0300

    Other

--- src/providers/palestrantes/palestrantes.ts
+++ src/providers/palestrantes/palestrantes.ts
@@ -6,9 +6,9 @@ import { BaseProvider } from '../base/base';
 
 @Injectable()
 export class PalestrantesProvider extends BaseProvider {
-  
-    getPath(): string {
-      return 'assets/data/palestrantes.json';
-    }
+
+  getPath(): string {
+    return 'assets/data/palestrantes.json';
+  }
 
 }
--- src/providers/programacao/programacao.ts
+++ src/providers/programacao/programacao.ts
@@ -1,18 +1,52 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 import { BaseProvider } from '../base/base';
+import { PalestrantesProvider } from '../palestrantes/palestrantes';
 
 @Injectable()
 export class ProgramacaoProvider extends BaseProvider {
-  
-    getPath(): string {
-      return 'assets/data/programacao.json';
-    }
 
-    getTimeline(dayIndex) {
-      return this.getData().map(data => {
-        return data[dayIndex];
+  private speakers = [];
+
+  constructor(public http: Http, public palProv: PalestrantesProvider) {
+    super(http);
+    this.palProv.getData().subscribe(speakers => {
+      this.speakers = speakers;
+    });
+  }
+
+  getPath(): string {
+    return 'assets/data/programacao.json';
+  }
+
+  getTimeline(dayIndex) {
+    return this.getData().map(data => {
+      let day: any = data[dayIndex];
+      let isOn: boolean = true;
+
+      day.groups.forEach(group => {
+        // loop through each session in the timeline group
+        group.sessions.forEach(session => {
+          session.opt = isOn ? "even" : "odd";
+          isOn = !isOn;
+          session.speakers = [];
+          if (session.speakerNames) {
+            session.speakerNames.forEach(speakerName => {
+              let speaker = this.speakers.find(s => s.name === speakerName);
+              if (speaker) {
+                session.speakers.push(speaker);
+                speaker.sessions = speaker.sessions || [];
+                speaker.sessions.push(session);
+              }
+            });
+          }
+
+        });
       });
-    }
+
+      return day;
+    });
+  }
 }
